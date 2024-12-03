12cdbf3b70fd5fcf4b849444a6cf29ccf81ddf47
Dalton Prokosch <103225424+garthable@users.noreply.github.com>
2024-12-03 09:53:45 -0700

    Fixed issue

--- src/imageProcessor.py
+++ src/imageProcessor.py
@@ -12,10 +12,8 @@ def read_image(path: str) -> np.ndarray[int] | None:
         return raw.postprocess()
     return None
 
-def write_image(image: np.ndarray[int], name: str, type: output_type):
+def write_image(image: np.ndarray[np.ndarray[int]], name: str, type: output_type) -> None:
     destination = "outputImages(encoded)/"
     if type == output_type.DECODED:
         destination = "outputImages(decoded)/"
-    with open(destination + name, "w") as file:
-        file.write("")
     iio.imwrite(destination + name + ".tiff", image)
