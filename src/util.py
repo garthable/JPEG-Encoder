065749fd8f0fd3b885c84d865b29802338a0488d
brendanharrington <brha8770@colorado.edu>
2024-12-06 14:25:11 -0700

    added to functions and images

---
+++ src/util.py
@@ -0,0 +1,6 @@
+import cv2 as cv
+
+filename = "../inputImages/google-chrome-icon.png"
+img = cv.imread(filename)
+cv.imshow("Display window", img)
+k = cv.waitKey(0) # Wait for a keystroke in the window
