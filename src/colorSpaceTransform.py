d819a0e576c3318e2245e8bb41a325f707a9af3f
Eldin Basic <elba8705@colorado.edu>
2024-12-11 19:28:13 -0700

    update testing

--- src/colorSpaceTransform.py
+++ src/colorSpaceTransform.py
@@ -1,7 +1,7 @@
 import numpy as np;
 from PIL import Image;
 
-# Function to convert RGB to YCbCr
+Function to convert RGB to YCbCr
 def rgb_to_ycbcr(rgb_image):
     transform_matrix = np.array([
         [0.299, 0.587, 0.114],
@@ -12,7 +12,7 @@ def rgb_to_ycbcr(rgb_image):
     ycbcr_image = np.dot(rgb_image, transform_matrix.T) + offset
     return ycbcr_image
 
-# Function to convert YCbCr back to RGB
+Function to convert YCbCr back to RGB
 def ycbcr_to_rgb(ycbcr_image):
     inverse_matrix = np.array([
         [1.0, 0.0, 1.402],
@@ -24,40 +24,40 @@ def ycbcr_to_rgb(ycbcr_image):
     rgb_image = np.clip(rgb_image, 0, 255).astype(np.uint8)
     return rgb_image
 
-def ycbcr_2_rgb(image):
-    return image
+#test the colorSpaceTransform
 
-#add file
-    
-filename = "../inputImages/pixel.png"
-img = Image.open(filename)
-    
-width = img.size
-height = img.size
-print('width :', width)
-print('height:', height)
+def colorSpaceTransformTest():
 
-rgb_img = img.convert('RGB')
+    fileName = input("Enter a filename followed by ../inputImages/")
+    img = Image.open(fileName)
 
-#Extracting the rgb values from each pixel then converting to YCbCr
+    width = img.size
+    height = img.size
+    print('width :', width)
+    print('height:', height)
 
-ycbcr_pixels = []
-count = 0
+    rgb_img = img.convert('RGB')
 
-for y in range(height):
-    for x in range(width):
-        pixel = rgb_img.getpixel((x, y))
-        r, g, b = pixel
+    #Extracting the rgb values from each pixel then converting to YCbCr
 
-        #for testing purposes
-        #print(f'| R: {r:3} | G: {g:3} | B: {b:3} |')#
+    ycbcr_pixels = []
+    count = 0
 
-        Y = 0.299 * r + 0.587 * g + 0.114 * b
-        Cb = -0.1687 * r - 0.3313 * g + 0.5 * b + 128
-        Cr = 0.5 * r - 0.4187 * g - 0.0813 * b + 128
-        ycbcr_pixels.append((Y, Cb, Cr))
-        count+=1
+    for y in range(height):
+        for x in range(width):
+            pixel = rgb_img.getpixel((x, y))
+            r, g, b = pixel
 
-        print(f'| Y: {Y:6.2f} | Cb: {Cb:6.2f} | Cr: {Cr:6.2f} |')
- 
-print(count)
+            #for testing purposes
+            print(f'| R: {r:3} | G: {g:3} | B: {b:3} |')
+
+            Y = 0.299 * r + 0.587 * g + 0.114 * b
+            Cb = -0.1687 * r - 0.3313 * g + 0.5 * b + 128
+            Cr = 0.5 * r - 0.4187 * g - 0.0813 * b + 128
+            ycbcr_pixels.append((Y, Cb, Cr))
+            count+=1
+
+            print(f'| Y: {Y:6.2f} | Cb: {Cb:6.2f} | Cr: {Cr:6.2f} |')
+
+    #counts the number of pixel iterations
+    print(count)
